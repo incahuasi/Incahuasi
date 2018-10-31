448d0f926c65396c1bb5ae40c759ea8ccc41396d
DennDums <44644481+DennDums@users.noreply.github.com>
2018-10-31 11:53:20 -0500

    Delete footer.php

--- footer.php
+++
@@ -1,4 +0,0 @@
-<footer id="main-footer"><p id="autor">&copy; Copyright Javier Jhonatan</p>
-             <p><a href="#">INCAHUASI-FERRE&Ntilde;AFE-LAMBAYEQUE-PER&Uacute;</a> - 2017</p>
-</footer>
-</html>
